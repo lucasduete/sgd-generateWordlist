a9ea2da33438873112bf36e60279ae63d739bd57
Lucas Duete <lm.duete@gmail.com>
2018-07-27 17:06:11 -0300

    Implementing technique of wordlist generation based on Fixed Letter.

---
+++ src/main/java/io/github/lucasduete/sgd/gererateWordlist/core/TechniqueFixedLetter.java
@@ -0,0 +1,51 @@
+package io.github.lucasduete.sgd.gererateWordlist.core;
+
+import io.github.lucasduete.sgd.gererateWordlist.factories.WordlistFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TechniqueFixedLetter implements GenerateTechnique {
+
+    @Override
+    public String[] generateWordlist() {
+        char[] textoChar = new char[0];
+
+        try {
+
+            textoChar = wordlistToCharArray();
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+            return new String[1];
+        }
+
+
+        List<String> wordlist = new ArrayList<>();
+
+        for (int i = 0; i < (textoChar.length - 7); i++)
+            wordlist.add(generateString(textoChar, i, 8));
+
+        return (String[]) wordlist.toArray();
+    }
+
+    private char[] wordlistToCharArray() throws IOException {
+        WordlistFactory factory = new WordlistFactory();
+        String[] wordlist = factory.generateWordlist();
+
+        return wordlist
+                .toString()
+                .toCharArray();
+    }
+
+    private static String generateString(char[] textoChar, int initialPosition, int quantCaracters) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < quantCaracters; i++)
+            stringBuilder.append(textoChar[initialPosition + i]);
+
+        return stringBuilder.toString();
+    }
+
+}
